09d6502c530a88cdc6d5d65fe57c8b47b3fe66b9
malakkhasawneh2 <malakkhasawneh2@gmail.com>
2021-09-13 19:04:03 +0300

    lap11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,8 +2,35 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import BookItem from './BookItem';
+
 
 class MyFavoriteBooks extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      favBooksArr : []
+    }
+  }
+
+  componentDidMount = () =>{
+    const { user } = this.props.auth0;
+    const email = user.email;
+    axios
+    .get(`http://localhost:3001/getBooks?email=${email}`)
+    .then( result =>{
+      this.setState({
+        favBooksArr:result.data
+      })
+    })
+    .catch (err =>{
+      console.log('error');
+    })
+  }
+
   render() {
     return(
       <Jumbotron>
@@ -11,9 +38,17 @@ class MyFavoriteBooks extends React.Component {
         <p>
           This is a collection of my favorite books
         </p>
+        {this.state.favBooksArr.map(item=>{
+          return(
+            <BookItem 
+            item = {item}
+            />
+          )
+        })
+       }
       </Jumbotron>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
